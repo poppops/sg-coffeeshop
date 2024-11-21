650629280589663497b5b3cd4396a51799b3fce7
Suhmayah Banda <s.banda911@googlemail.com>
2024-11-21 18:03:22 +0000

    Created helper functions to reduce code duplication
    - getSuccessResponse and getErrorResponse format responses and make code more readable

--- coffeeshop.js
+++ coffeeshop.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { DynamoDBClient, ScanCommand, GetItemCommand, PutItemCommand, DeleteItemCommand } = require("@aws-sdk/client-dynamodb")
+const { getSuccessResponse, getErrorResponse } = require("./lib/helper")
 
 exports.getAllItems = async (event) => {
 
@@ -12,26 +13,20 @@ exports.getAllItems = async (event) => {
         TableName: process.env.DYNAMODB_MENU_TABLE,
     }))
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(results.Items.map((item) => {
-            return {
-                menu_id: item.menu_id.S,
-                name: item.name.S,
-                description: item.description.S,
-                price: item.price.N,
-            }
-        })),
-        headers: {
-            "Content-Type": "application/json",
+    return getSuccessResponse(results.Items.map((item) => {
+        return {
+            menu_id: item.menu_id.S,
+            name: item.name.S,
+            description: item.description.S,
+            price: item.price.N,
         }
-    }
+    }))
+
 }
 
 module.exports.createMenuItem = async (event) => {
 
     const payload = JSON.parse(event.body)
-    console.log(payload)
 
     const client = new DynamoDBClient({
         region: process.env.AWS_REGION,
@@ -49,19 +44,9 @@ module.exports.createMenuItem = async (event) => {
 
     try {
         const data = await client.send(new PutItemCommand(params));
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: JSON.stringify(data),
-            }),
-        };
+        return getSuccessResponse(JSON.stringify(data))
     } catch (error) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                error: error,
-            }),
-        };
+        return getErrorResponse(error)
     }
 }
 
@@ -70,12 +55,7 @@ module.exports.getMenuItem = async (event) => {
     const requestParams = event.pathParameters
 
     if (!requestParams.menu_id) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: "Please specify a menu_id"
-            })
-        }
+        return getErrorResponse("Please specify a menu_id", 400)
     }
 
     const client = new DynamoDBClient({
@@ -92,33 +72,17 @@ module.exports.getMenuItem = async (event) => {
     try {
         const { Item } = await client.send(new GetItemCommand(query))
         if (!Item) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({
-                    error: "Item not found"
-                })
-            }
+            return getErrorResponse("Item not found", 400)
         } else {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    menu_id: Item.menu_id.S,
-                    name: Item.name.S,
-                    description: Item.description.S,
-                    price: Item.price.N,
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            }
+            return getSuccessResponse(JSON.stringify({
+                menu_id: Item.menu_id.S,
+                name: Item.name.S,
+                description: Item.description.S,
+                price: Item.price.N,
+            }))
         }
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: error
-            })
-        }
+        return getErrorResponse(error)
     }
 }
 
@@ -126,12 +90,7 @@ module.exports.deleteMenuItem = async (event) => {
     const requestParams = event.pathParameters
 
     if (!requestParams.menu_id) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: "Please specify a menu_id"
-            })
-        }
+        return getErrorResponse("Please specify a menu_id", 400)
     }
 
     const client = new DynamoDBClient({
@@ -147,21 +106,11 @@ module.exports.deleteMenuItem = async (event) => {
 
     try {
         const result = await client.send(new DeleteItemCommand(query))
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "Item was successfully deleted"
-            })
-        }
-
+        return getSuccessResponse({
+            message: "Item was successfully deleted",
+        })
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: "There was an error deleting this item: " + error
-            })
-        }
+        return getErrorResponse("There was an error deleting this item")
     }
 
 }
---
+++ menu.js
@@ -0,0 +1,62 @@
+[
+    {
+        "id": "1",
+        "name": "Espresso",
+        "price": 2.5,
+        "description": "A strong, concentrated coffee made by forcing hot water through finely-ground coffee beans."
+    },
+    {
+        "id": "2",
+        "name": "Americano",
+        "price": 3.0,
+        "description": "A coffee drink made by diluting a shot of espresso with hot water, giving it a similar strength to drip coffee."
+    },
+    {
+        "id": "3",
+        "name": "Cappuccino",
+        "price": 3.5,
+        "description": "Espresso topped with steamed milk and a thick layer of foamed milk. Often sprinkled with cocoa powder."
+    },
+    {
+        "id": "4",
+        "name": "Latte",
+        "price": 3.0,
+        "description": "Espresso mixed with steamed milk, typically topped with a small amount of foam. A smooth, creamy coffee."
+    },
+    {
+        "id": "5",
+        "name": "Mocha",
+        "price": 4.0,
+        "description": "A latte with chocolate syrup or cocoa powder, often topped with whipped cream. A great option for chocolate lovers."
+    },
+    {
+        "id": "6",
+        "name": "Flat White",
+        "price": 3.5,
+        "description": "Similar to a latte, but with a higher ratio of coffee to milk, resulting in a stronger flavor and a silky texture."
+    },
+    {
+        "id": "7",
+        "name": "Macchiato",
+        "price": 3.0,
+        "description": "A shot of espresso with a small amount of steamed milk or foam on top. Strong and bold, with a hint of creaminess."
+    },
+    {
+        "id": "8",
+        "name": "Cortado",
+        "price": 3.2,
+        "description": "A balanced drink made with equal parts espresso and steamed milk. Strong coffee flavor with a smooth texture."
+    },
+    {
+        "id": "9",
+        "name": "Irish Coffee",
+        "price": 5.0,
+        "description": "A coffee cocktail made with Irish whiskey, hot coffee, and sugar, topped with a layer of cream."
+    },
+    {
+        "id": "10",
+        "name": "Affogato",
+        "price": 4.5,
+        "description": "A dessert-style coffee made by pouring a shot of hot espresso over a scoop of vanilla ice cream."
+    }
+]
