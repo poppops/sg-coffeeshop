b6cb981d4fa8eb2cc2bd1ff90a9d681660da3f74
Suhmayah Banda <s.banda911@googlemail.com>
2024-11-23 20:07:56 +0000

    Clean Up
    Updated the response from each request to include the object that
    was operated on.
    
    Made the id numeric so it's easier to find in the browser.
    
    Introduced a getItemQuery convenience method to minimise code
    duplication in the handlers.

--- coffeeshop.js
+++ coffeeshop.js
@@ -1,7 +1,16 @@
 'use strict'
 
-const { DynamoDBClient, ScanCommand, GetItemCommand, PutItemCommand, DeleteItemCommand } = require("@aws-sdk/client-dynamodb")
+const { DynamoDBClient, ScanCommand, GetItemCommand, PutItemCommand, DeleteItemCommand, UpdateItemCommand } = require("@aws-sdk/client-dynamodb")
 const { getSuccessResponse, getErrorResponse } = require("./lib/helper")
+
+const getItemQuery = (item_id) => {
+    return {
+        TableName: process.env.DYNAMODB_MENU_TABLE,
+        Key: {
+            menu_id: { N: item_id }
+        }
+    }
+}
 
 exports.getAllItems = async (event) => {
 
@@ -15,7 +24,7 @@ exports.getAllItems = async (event) => {
 
     return getSuccessResponse(results.Items.map((item) => {
         return {
-            menu_id: item.menu_id.S,
+            menu_id: item.menu_id.N,
             name: item.name.S,
             description: item.description.S,
             price: item.price.N,
@@ -35,16 +44,22 @@ module.exports.createMenuItem = async (event) => {
     const params = {
         TableName: process.env.DYNAMODB_MENU_TABLE,
         Item: {
-            "menu_id": { S: payload.name.replace(" ", "_") },
+            "menu_id": { N: payload.menu_id },
             "name": { S: payload.name },
             "description": { S: payload.description },
             "price": { N: payload.price },
-        }
+        },
     };
 
     try {
-        const data = await client.send(new PutItemCommand(params));
-        return getSuccessResponse(JSON.stringify(data))
+        await client.send(new PutItemCommand(params));
+        const { Item } = await client.send(new GetItemCommand(getItemQuery(payload.menu_id)))
+        return getSuccessResponse({
+            menu_id: Item.menu_id.N,
+            name: Item.name.S,
+            description: Item.description.S,
+            price: Item.price.N,
+        })
     } catch (error) {
         return getErrorResponse(error)
     }
@@ -62,27 +77,62 @@ module.exports.getMenuItem = async (event) => {
         region: process.env.AWS_REGION,
     })
 
-    const query = {
-        TableName: process.env.DYNAMODB_MENU_TABLE,
-        Key: {
-            menu_id: { S: requestParams.menu_id }
-        }
-    }
-
     try {
-        const { Item } = await client.send(new GetItemCommand(query))
+        const { Item } = await client.send(new GetItemCommand(getItemQuery(requestParams.menu_id)))
         if (!Item) {
             return getErrorResponse("Item not found", 400)
         } else {
-            return getSuccessResponse(JSON.stringify({
-                menu_id: Item.menu_id.S,
+            return getSuccessResponse({
+                menu_id: Item.menu_id.N,
                 name: Item.name.S,
                 description: Item.description.S,
                 price: Item.price.N,
-            }))
+            })
         }
     } catch (error) {
         return getErrorResponse(error)
+    }
+}
+
+module.exports.updateMenuItem = async (event) => {
+    const requestParams = event.pathParameters
+    const payload = JSON.parse(event.body)
+
+    if (!requestParams.menu_id) {
+        return getErrorResponse("Please specify a menu_id", 400)
+    }
+
+    const client = new DynamoDBClient({
+        region: process.env.AWS_REGION,
+    })
+
+    const query = {
+        ...getItemQuery(requestParams.menu_id),
+        UpdateExpression: "SET #name = :name, #price = :price, #description = :description",
+        ExpressionAttributeNames: {
+            "#name": "name",
+            "#price": "price",
+            "#description": "description",
+        },
+        ExpressionAttributeValues: {
+            ":name": { S: payload.name },
+            ":price": { N: payload.price },
+            ":description": { S: payload.description },
+        },
+        ReturnValues: "ALL_NEW",
+    }
+
+    try {
+        const result = await client.send(new UpdateItemCommand(query))
+        const item = result.Attributes
+        return getSuccessResponse({
+            menu_id: item.menu_id.N,
+            name: item.name.S,
+            description: item.description.S,
+            price: item.price.N,
+        })
+    } catch (error) {
+        return getErrorResponse("There was an error updating this item: " + error)
     }
 }
 
@@ -97,20 +147,12 @@ module.exports.deleteMenuItem = async (event) => {
         region: process.env.AWS_REGION,
     })
 
-    const query = {
-        TableName: process.env.DYNAMODB_MENU_TABLE,
-        Key: {
-            menu_id: { S: requestParams.menu_id }
-        }
-    }
-
     try {
-        const result = await client.send(new DeleteItemCommand(query))
+        const result = await client.send(new DeleteItemCommand(getItemQuery(requestParams.menu_id)))
         return getSuccessResponse({
             message: "Item was successfully deleted",
         })
     } catch (error) {
         return getErrorResponse("There was an error deleting this item")
     }
-
 }
